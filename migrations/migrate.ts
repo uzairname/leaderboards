b18a3ba875d04910099068243e93d01e62437415
uzairname <23709618+uzairname@users.noreply.github.com>
2023-12-11 23:36:44 -0500

    .

--- migrations/migrate.ts
+++ migrations/migrate.ts
@@ -4,16 +4,17 @@ import { migrate } from 'drizzle-orm/postgres-js/migrator'
 import * as dotenv from 'dotenv'
 
 async function migrate_database() {
-  dotenv.config()
   console.log(process.env.POSTGRES_URL)
   if (!process.env.POSTGRES_URL) throw new Error('POSTGRES_URL not set')
-
+  
   console.log('migrating')
   const db = drizzle(postgres(process.env.POSTGRES_URL, { ssl: 'require', max: 1 }))
   await migrate(db, { migrationsFolder: 'migrations/migrations' })
-  console.log('done')
 }
 
+dotenv.config()
+
 migrate_database().then(() => {
+  console.log('done')
   process.exit(0)
 })
